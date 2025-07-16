ebfd4fb2beadc367151f012848ba7d8fc6effc53
HarishWiz <harishhaish8122@gmail.com>
2025-07-16 19:58:48 +0530

    sign in fetch changed

--- Front-end/src/Components/SignIn.jsx
+++ Front-end/src/Components/SignIn.jsx
@@ -26,7 +26,7 @@ const SignIn = () => {
     setLoading(true);
     try {
       await new Promise((resolve) => setTimeout(resolve, 2000));
-      const res = await fetch( `${process.env.REACT_APP_API_URL}/signin`, {
+      const res = await fetch( `https://fullstackproject-1-ruan.onrender.com/api/signin`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
